6d2db34803a5c4c4629638026d284578d7f8adf2
Jesse Stover <j.stover.r@gmail.com>
2024-04-19 10:43:24 +0900

    flask app skeleton

---
+++ l2ai/config.py
@@ -0,0 +1,27 @@
+import os
+
+
+class Default:
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'secret')
+    DEBUG = os.getenv('FLASK_DEBUG', True)
+    LOG_LEVEL = os.getenv('FLASK_LOG_LEVEL', 'INFO')
+    SQLALCHEMY_DATABASE_URI = os.getenv('FLASK_DB')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class Development(Default):
+    ENV = 'development'
+
+
+class Production(Default):
+    ENV = 'production'
+
+
+class Debug(Default):
+    DEBUG = True
+    LOG_LEVEL = 'DEBUG'
+
+
+class Testing(Default):
+    ENV = 'development'
+    TESTING = True
---
+++ run.py
@@ -0,0 +1,6 @@
+from l2ai.app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)

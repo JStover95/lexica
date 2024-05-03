2d5058f156469b710f0fb4ab826440b6c40075af
Jesse Stover <j.stover.r@gmail.com>
2024-05-03 18:07:31 +0900

    handlers.py commented

--- l2ai/utils/handlers.py
+++ l2ai/utils/handlers.py
@@ -4,6 +4,16 @@ from l2ai.utils.logging import logger
 
 
 def handle_client_error(e):
+    """
+    Handle AWS boto3 client errors. Get the error's code and message, create a
+    log output, and raise a RuntimeError.
+
+    Args:
+        e (_type_)
+
+    Raises:
+        RuntimeError
+    """
     try:
         code = e.response["Error"]["Code"]
 
@@ -25,11 +35,28 @@ def handle_server_error(
         code: int,
         e: Exception | None = None
     ) -> Response:
+    """
+    Handle errors in server endpoints. Generate a logging message and return the
+    a response with a message to the client. The message will also be logged.
+    The message string can include the following formatting keys:
+
+     - %(exc)s: the formatted exception.
+
+    Args:
+        msg (str): The message to return to the client with optional formatting
+            keys
+        code (int): The response code to return to the client (e.g., 401, 500)
+        e (Exception | None, optional): The exception. Defaults to None
+
+    Returns:
+        Response: The response to return to the client.
+    """
     if e is not None:
-        logger.error(msg)
-        logger.exception(e)
         exc = traceback.format_exception_only(e).pop().strip()
-        res = {"Message": msg % {"exc": exc}}
+        s = msg % {"exc": exc}
+        logger.error(s)
+        logger.exception(e)
+        res = {"Message": s}
 
     else:
         res = {"Message": msg}

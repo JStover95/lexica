4449b0b7621fc48de7afbccbfb8758f39472e93d
Jesse Stover <j.stover.r@gmail.com>
2024-07-23 22:20:10 -0700

    deleted collections

--- l2ai/schema.py
+++ l2ai/schema.py
@@ -5,52 +5,52 @@ from l2ai import models
 
 class EquivalentType(MongoengineObjectType):
     class Meta:
-        model = models.EquivalentModel
+        model = models.Equivalent
 
 
 class ContentSurfacesType(MongoengineObjectType):
     class Meta:
-        model = models.ContentSurfacesModel
+        model = models.ContentSurfaces
 
 
 class ContentIxType(MongoengineObjectType):
     class Meta:
-        model = models.ContentIxModel
+        model = models.ContentIx
 
 
 class ExplanationType(MongoengineObjectType):
     class Meta:
-        model = models.ExplanationModel
+        model = models.Explanation
 
 
 class HighlightType(MongoengineObjectType):
     class Meta:
-        model = models.HighlightModel
+        model = models.Highlight
 
 
 class SenseRankType(MongoengineObjectType):
     class Meta:
-        model = models.SenseRankModel
+        model = models.SenseRank
 
 
 class UserType(MongoengineObjectType):
     class Meta:
-        model = models.UserModel
+        model = models.User
 
 
 class SenseType(MongoengineObjectType):
     class Meta:
-        model = models.SenseModel
+        model = models.Sense
 
 
 class DictionaryEntryType(MongoengineObjectType):
     class Meta:
-        model = models.DictionaryEntryModel
+        model = models.DictionaryEntry
 
 
 class ContentType(MongoengineObjectType):
     class Meta:
-        model = models.ContentModel
+        model = models.Content
 
 
 class Query(graphene.ObjectType):
@@ -65,16 +65,16 @@ class Query(graphene.ObjectType):
     content_by_id = graphene.Field(ContentType, id=graphene.String(required=True))
 
     def resolve_user_by_id(self, info, id):
-        return models.UserModel.objects.get(id=id)
+        return models.User.objects.get(id=id)
 
     def resolve_dictionary_entry_by_id(self, info, id):
-        return models.DictionaryEntryModel.objects.get(id=id)
+        return models.DictionaryEntry.objects.get(id=id)
 
     def resolve_sense_by_id(self, info, id):
-        return models.SenseModel.objects.get(id=id)
+        return models.Sense.objects.get(id=id)
 
     def resolve_content_by_id(self, info, id):
-        return models.ContentModel.objects.get(id=id)
+        return models.Content.objects.get(id=id)
 
 
 
@@ -85,7 +85,7 @@ class CreateUser(graphene.Mutation):
     user = graphene.Field(UserType)
 
     def mutate(self, info, username):
-        user = models.UserModel(username=username)
+        user = models.User(username=username)
         user.save()
         return CreateUser(user=user)
 

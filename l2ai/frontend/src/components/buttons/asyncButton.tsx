18e1ee65de7bc94f9f2687c6bd0b706faeadcf8f
Jesse Stover <j.stover.r@gmail.com>
2024-08-21 17:26:22 -0400

    convert asyncButton to function component

--- l2ai/frontend/src/components/buttons/asyncButton.tsx
+++ l2ai/frontend/src/components/buttons/asyncButton.tsx
@@ -1,58 +1,41 @@
-import * as React from "react";
-import { Component } from "react";
+import React, { useState } from "react";
 
-/**
- * onClick: the function to handle clicks
- * text: the text to display in the button
- * type: the button's type (primary, secondary, etc.)
- */
 interface AsyncButtonProps {
   onClick: () => Promise<any>;
   text: string;
   type: string;
 }
 
-/**
- * loading: whether to show the loader
- */
-interface AsyncButtonState {
-  loading: boolean;
-}
+const AsyncButton: React.FC<AsyncButtonProps> = ({ onClick, text, type }) => {
+  const [loading, setLoading] = useState<boolean>(false);
 
-class AsyncButton extends React.Component<AsyncButtonProps, AsyncButtonState> {
-  state = { loading: false };
-
-  /**
-   * Show the loader, call the onClick function, and hide the loader when the
-   * promise is complete
-   */
-  handleClick = (): void => {
-    this.setState({ loading: true }, () =>
-      this.props.onClick().then(() => this.setState({ loading: false }))
-    );
+  const handleClick = async () => {
+    setLoading(true);
+    try {
+      await onClick();
+    } finally {
+      setLoading(false);
+    }
   };
 
-  render() {
-    const { text, type } = this.props;
+  const loader = (
+    <div className="lds-ring lds-ring-small">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 
-    const loader = (
-      <div className="lds-ring lds-ring-small">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    );
-
-    return (
-      <button
-        className={"button button-large button-" + type}
-        onClick={this.handleClick}
-      >
-        {this.state.loading ? loader : text}
-      </button>
-    );
-  }
-}
+  return (
+    <button
+      className={`button button-large button-${type}`}
+      onClick={handleClick}
+      disabled={loading}
+    >
+      {loading ? loader : text}
+    </button>
+  );
+};
 
 export default AsyncButton;

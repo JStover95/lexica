0be6eff9cba1681012e02fefa40c2339bf79233c
Jesse Stover <j.stover.r@gmail.com>
2024-10-03 18:18:30 -0400

    Animated arrow on open/close seen content

--- app/frontend/src/components/read/phrases/dictionaryQuery.tsx
+++ app/frontend/src/components/read/phrases/dictionaryQuery.tsx
@@ -1,8 +1,9 @@
-import React, { RefObject, useState } from "react";
+import React, { createRef, RefObject, useState } from "react";
 import { IDictionaryQuery } from "../../../utils/interfaces";
 import { highlightSubstrings } from "../../../utils/utils";
 import SeenContent from "./seenContent";
 import DictionaryEntriesContainer from "./dictionaryEntriesContainer";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 interface IDictionaryQueryProps {
   dictionaryQuery: IDictionaryQuery;
@@ -16,6 +17,18 @@ const DictionaryQuery: React.FC<IDictionaryQueryProps> = ({
 }) => {
   const [openSeenContent, setOpenSeenContent] = useState(false);
   const { query, entries, seenContent } = dictionaryQuery;
+  const arrowRef = createRef<HTMLDivElement>();
+
+  const handleClickSeenContent = () => {
+    if (arrowRef.current) {
+      if (openSeenContent) {
+        arrowRef.current.style.transform = "rotate(0deg)";
+      } else {
+        arrowRef.current.style.transform = "rotate(180deg)";
+      }
+    }
+    setOpenSeenContent(!openSeenContent);
+  };
 
   const seen = seenContent.map((sc, i) =>
     highlightSubstrings(sc.text, sc.indices).map((elem, j) =>
@@ -35,11 +48,18 @@ const DictionaryQuery: React.FC<IDictionaryQueryProps> = ({
         <span className="text-lg">{query}</span>
         {
           Boolean(seenContent.length) &&
-          <span
-            onClick={() => setOpenSeenContent(!openSeenContent)}
-            className="text-sm underline">
-              Seen {numSeen} time{numSeen > 1 && "s"}
-          </span>
+          <div
+            onClick={handleClickSeenContent}
+            className="flex items-center">
+              <span className="text-sm underline">
+                  Seen {numSeen} time{numSeen > 1 && "s"}
+              </span>
+              <div
+                ref={arrowRef}
+                className="flex items-center transition-all duration-300">
+                  <KeyboardArrowDownIcon sx={{ fontSize: "20px" }} />
+              </div>
+          </div>
         }
       </div>
       <SeenContent open={openSeenContent}>
